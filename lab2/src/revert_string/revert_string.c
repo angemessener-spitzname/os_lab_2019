4045d3ea0b96f02e4ba2359dc69c42802eb41bd9
angemessener-spitzname <svetlanarysina7@gmail.com>
2021-04-24 00:31:29 +0000

    revert_string.c completed

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,16 @@
 #include "revert_string.h"
-
+#include "stdio.h"
+#include "malloc.h"
+#include "string.h"
 void RevertString(char *str)
 {
-	// your code here
+char temp;
+    int k = 0;
+    int i;
+    for ( i = 0 ;i < strlen(str) / 2; i++)
+    {
+        temp = str[i];
+        str[i] = str[strlen(str) - 1 - i];
+        str[strlen(str) - 1 - i] = temp;
+    }
 }
-
