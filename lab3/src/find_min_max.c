175a545b1ab249600e11737ff70a024e9847ebfb
angemessener-spitzname <svetlanarysina7@gmail.com>
2021-05-20 18:57:24 +0000

    task1

--- lab3/src/find_min_max.c
+++ lab3/src/find_min_max.c
@@ -1,5 +1,5 @@
 #include "find_min_max.h"
-
+#include "stdio.h"
 #include <limits.h>
 
 struct MinMax GetMinMax(int *array, unsigned int begin, unsigned int end) {
@@ -7,6 +7,30 @@ struct MinMax GetMinMax(int *array, unsigned int begin, unsigned int end) {
   min_max.min = INT_MAX;
   min_max.max = INT_MIN;
 
-  // your code here
+int i;
+//вывод массива, для того, чтобы убедиться, что все работает верно
+for ( i = begin; i < end; i++)
+{
+    printf("%d",array[i]);
+    printf("\t");
+}
+printf("\n");
+ for ( i = begin; i < end; i++)
+ {
+    if(min_max.min > array[i])
+        min_max.min = array[i];
+    else 
+        continue;
+    
+ }
+
+ for ( i = begin; i < end; i++)
+ {
+    if(min_max.max < array[i])
+        min_max.max = array[i];
+    else 
+        continue;
+    
+ }
   return min_max;
 }
